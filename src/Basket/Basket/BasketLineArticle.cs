a73ae50bf069f8c4c3c62bcc47f629dc2c1ad747
vivienmahiette <vivien.mahiette@epsi.fr>
2018-05-04 16:23:06 +0200

    MAJ
    MAJ

---
+++ src/Basket/Basket/BasketLineArticle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket
+{
+    /* class BasketLineArticle
+     {
+     }*/
+    public struct BasketLineArticle
+    {
+        public string Id { get; set; }
+        public int Number { get; set; }
+        public string Label { get; set; }
+    }
+}
---
+++ src/Basket/BasketTests/BasketOperation_CalculateBasketAmoutShould.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Basket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasketTests
+{
+    /* class BasketOperation_CalculateBasketAmoutShould
+     {
+     }*/
+    [TestClass]
+    public class BasketOperation_CalculateBasketAmoutShould
+    {
+        public class BasketTest
+        {
+            public List<BasketLineArticle> BasketLineArticles { get; set; }
+            public int ExpectedPrice { get; set; }
+        }
+        private static IEnumerable<object[]> Baskets
+        {
+            get
+            {
+                return new[]
+                { new object[] {
+                        new BasketTest(){ BasketLineArticles = new List<BasketLineArticle>
+                            {
+                                new BasketLineArticle {Id = "1", Number = 12, Label = "Banana"},
+                                new BasketLineArticle {Id = "2", Number = 1, Label = "Fridge electrolux"},
+                                    
+                                    new BasketLineArticle {Id = "3", Number = 4, Label = "Chair"}
+                            },
+                            ExpectedPrice = 84868}
+                        },
+                        new object[] {
+                            new BasketTest(){ BasketLineArticles = new List<BasketLineArticle>
+                                {
+                                    new BasketLineArticle {Id = "1", Number = 20, Label = "Banana"},
+                                    new BasketLineArticle {Id = "3", Number = 6, Label = "Chair"}
+                                },
+                                ExpectedPrice = 37520}
+                        },
+                    };
+                }
+            }
+            [TestMethod]
+            [DynamicData("Baskets")]
+            public void ReturnCorrectAmoutGivenBasket(BasketTest basketTest)
+        {
+            var amountTotal = 0;
+            Assert.AreEqual(amountTotal, basketTest.ExpectedPrice);
+        }
+    }
+}
