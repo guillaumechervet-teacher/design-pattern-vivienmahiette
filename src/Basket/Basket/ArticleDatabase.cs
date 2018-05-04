f18997c502112b3e2c44e4841d6cd1ccf42901f6
vivienmahiette <vivien.mahiette@epsi.fr>
2018-05-04 16:07:42 +0200

    Mise en place
    Mise en place des fichiers

---
+++ src/Basket/Basket/ArticleDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket
+{
+    /* class ArticleDatabase
+     {
+     }*/
+    public struct ArticleDatabase
+    {
+        public string Id { get; set; }
+        public string Label { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
